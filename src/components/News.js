539426c77318f8cf586c4bdb0d55d860290196b5
prasanna3511 <80350696+prasanna3511@users.noreply.github.com>
2023-01-11 12:00:42 +0530

    dfgd

--- src/components/News.js
+++ src/components/News.js
@@ -11,11 +11,47 @@ export class News extends Component {
     }
   }
    async componentDidMount(){
-    let url= "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=28a7460a672940eab62e0f74f35ddef2";
+    let url= "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=28a7460a672940eab62e0f74f35ddef2&page=0";
     let data = await fetch(url);
     let pData = await data.json()
-    this.setState({articles: pData.articles})
+    this.setState({articles: pData.articles , totalresult:pData.totalResults})
     console.log(pData)
+   }
+
+   handlenextClick= async ()=> {
+      if(this.state.page + 1 >Math.ceil(this.state.totalResults/20))
+      {
+
+      }
+      else{
+
+    
+      let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=28a7460a672940eab62e0f74f35ddef2&page=${this.state.page + 1}&pagesize=20`;
+      let data = await fetch(url);
+      let pData = await data.json()
+
+
+      this.setState({
+      
+      page: this.state.page + 1,
+      articles: pData.articles
+    })
+  }
+
+   }
+   handleprvsClick= async ()=>{
+    let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=28a7460a672940eab62e0f74f35ddef2&page=${this.state.page - 1}&pagesize=20`;
+      let data = await fetch(url);
+      let pData = await data.json()
+
+
+      this.setState({
+      
+      page: this.state.page - 1,
+      articles: pData.articles
+    }
+      )
+
    }
  
   render() {
@@ -24,11 +60,15 @@ export class News extends Component {
         <h1>Daily news</h1>
         <div className="row" >
         {this.state.articles?.map((element)=>{
-  return  <div className="col-md-4" Key={element.url}>
+  return  <div className="col-md-4" key={element.url}>
           <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:""} newsUrl={element.url?element.url:""} target="_blank" className="card-img-top" alt="..."/>
           </div>
           
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handleprvsClick}> &larr; previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handlenextClick}>next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,13 +6,13 @@ export class NewsItem extends Component {
     return (
       <div className="my-4">
         <div className="card" style={{width: "18rem"}}>
-          <img src={!imageUrl?"https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/303b3b701ab97ee00ab87508ca1d260a.jpg":imageUrl} />
+          <img src={!imageUrl?"https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/303b3b701ab97ee00ab87508ca1d260a.jpg":imageUrl} alt="" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-          <a href={newsUrl} className="btn btn-sm btn-primary">
+          <a href={newsUrl} className="btn btn-sm btn-dark">
               read More
             </a>
           </div>
